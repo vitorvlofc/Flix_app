e729d8d0bbd9a2ce30c22ccc08ff41113cbdfc5f
vitor <vitorvlofc@users.noreply.github.com>
2026-03-23 22:00:15 -0300

    003 - Telas e menu

--- app.py
+++ app.py
@@ -1,7 +1,26 @@
 import streamlit as st
 
 
-st.title("Flix App")
-st.divider()
-st.text_input("Nome do Filme")
-st.button("Pesquisar")
+def main():
+    st.title("Flix App")
+
+    menu_options = st.sidebar.selectbox("Selecione uma opção", ["Início", "Gêneros", "Atores/Atrizes", "Avaliações"])
+
+    if menu_options == "Início":
+        st.write("  início")
+
+    if menu_options == "Gêneros":
+        st.write("  Lista de Gêneros")
+
+    if menu_options == "Atores/Atrizes":
+        st.write("  Lista de Atores/Atrizes")
+
+    if menu_options == "Filmes":
+        st.write("  Lista de Filmes")
+
+    if menu_options == "Avaliações":
+        st.write("  Lista de Avaliações")
+        
+
+if __name__ == "__main__":
+    main()
