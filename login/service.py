c26e37762a922d97ad8873a6f1b49057c7ae8ab1
vitor <vitorvlofc@users.noreply.github.com>
2026-03-29 19:24:18 -0300

    008 - Implementando o login (parte 1)

---
+++ login/service.py
@@ -0,0 +1,13 @@
+import streamlit as st
+from api.service import Auth
+
+
+def login(username, password):
+    Auth_service = Auth()
+    response = Auth_service.get_token(username, password)
+
+    if response.get('Error'):
+        st.error(f'Falhaa ao realizar login: {response.get("Error")}]')
+    else:
+        st.session_state.token = response.get('access')
+        st.rerun()
