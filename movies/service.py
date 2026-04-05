4fc99d091c1ea6c58c2d2bab5ceb4fab0ccd82e7
vitor <vitorvlofc@users.noreply.github.com>
2026-04-05 20:00:53 -0300

    019 - Criando service de movies

---
+++ movies/service.py
@@ -0,0 +1,24 @@
+from movies.repository import MoviesRepository
+
+
+class MoviesService:
+    def __init__(self):
+        self.movies_repository = MoviesRepository()
+
+
+    def get_movies(self):
+        return self.movies_repository.get_movies()
+    
+
+    def create_movie(self, title, release_date, genre, actors, resume):
+        movie = {
+            'title': title,
+            'release_date': release_date,
+            'genre': genre,
+            'actors': actors,
+            'resume': resume
+        }
+        return self.movies_repository.create_movie(movie)
+    
+
+    
